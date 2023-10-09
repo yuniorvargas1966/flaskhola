6139db6cfb1c447aadc96f4966c783ddeb2ec386
libreria <yuniorvargas1966@gmail.com>
2023-10-09 13:29:45 -0400

    5 Commit

--- static/js/index.js
+++ static/js/index.js
@@ -1,6 +1,6 @@
 const app = Vue.createApp({})
  app.component('component-a', {
-     template: `<p>Version 003. Estamos implementando una aplicacion con Flask en el backend y Vue en e frontend con render.com!           
+     template: `<p>Version 0.0.4</p><p>Estamos implementando una aplicacion con Flask en el backend </p><p>Vue en e frontend con render.com!</p><p>Bootstrap CSS           
 </p>`,
 })
 app.mount('#app')  

a1bb1c1d6f2135bf00f624fc2ce1293bf9eff8dd
Yunior Vargas Hernández <yuniorvargas1966@gmail.com>
2024-07-06 02:52:42 -0400

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -1,6 +1,6 @@
 const app = Vue.createApp({})
  app.component('component-a', {
-     template: `<p>Version 0.0.4</p><p>Estamos implementando una aplicacion con Flask en el backend </p><p>Vue en e frontend con render.com!</p><p>Nos sentimos contentos y satisfechos.          
+     template: `<p>Version 0.0.5</p><p>Estamos implementando una aplicacion con Flask versión 3.0.3 en el backend </p><p>Vue versión 3.4.31 en el frontend con render.com!</p><p>Nos sentimos contentos y satisfechos.          
 </p>`,
 })
 app.mount('#app')  
